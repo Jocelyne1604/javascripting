c866e24b51dda206bcfcc7e6deb52ab0269a4c3c
joydev1604 <lyne1604@gmail.com>
2019-08-30 18:19:24 +0200

    Practicing refactoring

--- arguments.js
+++ arguments.js
@@ -9,3 +9,11 @@ function area(lenght, width) {
 }
 
 area(10, 30);
+
+function greet(person1, person2, person3) {
+  console.log("hi " + person1);
+  console.log("hi " + person2);
+  console.log("hi " + person3);
+}
+
+greet("Harry", "Ron", "Hermione");
